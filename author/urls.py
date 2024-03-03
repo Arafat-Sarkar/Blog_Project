67749ba18984dd38dc328f55d5e6498f914c0a44
Arafat_Sarkar <arafatsarker286@gmail.com>
2024-03-03 11:41:59 +0600

    logout change

--- author/urls.py
+++ author/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
    
     path('registration/',UserRegistrationView.as_view(), name= 'registration' ),
     path('login/', UserLoginView.as_view(), name= 'login'),
-    path('logout/', UserLogoutView.as_view(), name= 'logout'),
+    # path('logout/', UserLogoutView.as_view(), name= 'logout'),
+    path('logout/',  user_logout, name= 'logout'),
     path('active/<uid64>/<token>', activate, name = 'active'),
     # path('registration/',register, name= 'registration'),
     
